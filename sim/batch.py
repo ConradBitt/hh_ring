96e5403305881269b5fdc568cace122c70cc1310
ConradBitt <conrad.bittencourt@gmail.com>
2023-10-27 17:54:07 -0300

    Antes rodar no servidor

--- sim/batch.py
+++ sim/batch.py
@@ -22,13 +22,9 @@ def custom():
     params = specs.ODict()
     
     # params[('seeds', 'conn')] =  [1] 
-    # params[('gex')] = [0.0005, 0.0010, 0.0015, 0.0020]
     # params[('IClamp0', 'amp')] = [0.08, 0.10, 0.12] 
-
-    params[('gex')] = [float(sys.argv[3])]  #v1
-    # filtering string received as argv parameter
-    arg_conns = [value.replace('[','').replace(']','').replace(',','').replace("'",'') for value in sys.argv[4:]]
-    params[('n_neighbors')] = np.array([int(value) for value in arg_conns if value != '']).astype('int64')
+    params[('gex')] = [0.0005, 0.0010]
+    params[('n_neighbors')] = [vv for vv in range(10,30,10)]
 
     b = Batch(params=params, netParamsFile='netParams.py', cfgFile='cfg.py')
 
@@ -61,26 +57,63 @@ def setRunCfg(b, type='mpi_bulletin'):
             'walltime': '24:00:00',
             'nodes': 1,
             'coresPerNode': 80,
-            'email': 'fernandodasilvaborges@gmail.com',
-            'folder': '/home/ext_fernandodasilvaborges_gmail_/S1_mouse/sim/',
+            'email': 'conrad.bittencourt@gmail.com',
+            'folder': '/home/ext_conrad.bittencourt_gmail_/S1_mouse/sim/',
             'script': 'init.py',
             'mpiCommand': 'mpirun',
             'skipCustom': '_raster.png'}
+        
+    elif type == 'hpc_slurm_Expanse':
+        b.runCfg = {'type': 'hpc_slurm',
+                    'allocation': 'TG-IBN140002',
+                    'partition': 'compute',
+                    'walltime': '1:00:00',
+                    'nodes': 1,
+                    'coresPerNode': 128,
+                    'email': 'conrad.bittencourt@gmail.com',
+                    'folder': '/home/fborges/hh_ring/sim/',
+                    'script': 'init.py',
+                    'mpiCommand': 'mpirun',
+                    'custom': '#SBATCH --mem=249325M\n#SBATCH --export=ALL\n#SBATCH --partition=compute',
+                    'skip': True}
+        
+    elif type == 'hpc_slurm_Expanse_debug':
+        b.runCfg = {'type': 'hpc_slurm',
+                    'allocation': 'TG-IBN140002',
+                    'partition': 'debug',
+                    'walltime': '1:00:00',
+                    'nodes': 1,
+                    'coresPerNode': 4,
+                    'email': 'conrad.bittencourt@gmail.com',
+                    'folder': '/home/fborges/hh_ring/sim/',
+                    'script': 'init.py',
+                    'mpiCommand': 'mpirun',
+                    'custom': '#SBATCH --mem=249325M\n#SBATCH --export=ALL\n#SBATCH --partition=debug',
+                    'skip': True}
 
 # ----------------------------------------------------------------------------------------------
 # Main code
 # ----------------------------------------------------------------------------------------------
+# if __name__ == '__main__': 
+#     b = custom() #
+#     # 
+#     version_number = sys.argv[1]
+#     batch_number = sys.argv[2]
+#     batch_number = batch_number.zfill(4) # fill string with zeros.
+
+#     b.batchLabel = f'v{version_number}_batch{batch_number}' #cfg.simLabel  # default: 'v0_batch0'
+        
+#     cfg.simLabel = b.batchLabel
+#     b.saveFolder = '../data/'+b.batchLabel
+#     b.method = 'grid'
+#     setRunCfg(b, 'mpi_direct')     # setRunCfg(b, 'mpi_bulletin')
+#     b.run() # run batch
+
 if __name__ == '__main__': 
     b = custom() #
-    # 
-    version_number = sys.argv[1]
-    batch_number = sys.argv[2]
-    batch_number = batch_number.zfill(4) # fill string with zeros.
 
-    b.batchLabel = f'v{version_number}_batch{batch_number}' #cfg.simLabel  # default: 'v0_batch0'
-        
-    cfg.simLabel = b.batchLabel
+    b.batchLabel = 'v1_batch0'  
     b.saveFolder = '../data/'+b.batchLabel
     b.method = 'grid'
-    setRunCfg(b, 'mpi_direct')     # setRunCfg(b, 'mpi_bulletin')
+    setRunCfg(b, 'mpi_direct') # hpc_slurm_Expanse  setRunCfg(b, 'hpc_slurm_Cineca_debug') # setRunCfg(b, 'hpc_slurm_Expanse')
     b.run() # run batch
